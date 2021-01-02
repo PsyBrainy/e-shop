758c5371d151e3d941ddc3841013467ff3e2e6b5
PsyBrainy <martuto93@gmail.com>
2021-01-02 16:57:42 -0300

    endpoint products

--- src/main/java/com/psybrainy/eshop/web/controller/ProductController.java
+++ src/main/java/com/psybrainy/eshop/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.psybrainy.eshop.web.controller;
 import com.psybrainy.eshop.domain.Product;
 import com.psybrainy.eshop.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,23 +15,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/all")
     public List<Product> getAll(){
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId){
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId){
         return productService.getProductById(productId);
     }
 
-    public Optional<List<Product>> getByName(String name){
+    @GetMapping("/name/{name}")
+    public Optional<List<Product>> getByName(@PathVariable("name") String name){
         return productService.getByName(name);
     }
 
-    public Product save(Product product){
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
         return productService.save(product);
     }
 
-    public boolean delete(int productId){
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int productId){
         return productService.delete(productId);
     }
 
